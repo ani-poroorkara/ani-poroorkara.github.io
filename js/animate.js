e8a2c2e93b6459c93b3565bdcce55424c5496350
ani-poroorkara <anirudh_p@outlook.com>
2021-09-10 00:43:28 +0530

    Restructuring HTML and removing themes

--- js/animate.js
+++ js/animate.js
@@ -2,23 +2,23 @@
 AOS.init();
 
 // Detect mobile interface
-var menu_btn = document.querySelector("#menu-btn");
-var sidebar = document.querySelector("#sidebar");
-var container = document.querySelector(".my-container");
+// var menu_btn = document.querySelector("#menu-btn");
+// var sidebar = document.querySelector("#sidebar");
+// var container = document.querySelector(".my-container");
 
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-  sidebar.classList.toggle("active-nav");
-  container.classList.toggle("active-cont");
-}
-menu_btn.addEventListener("click", () => {
-  sidebar.classList.toggle("active-nav");
-  container.classList.toggle("active-cont");
-});
+// if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+//   sidebar.classList.toggle("active-nav");
+//   container.classList.toggle("active-cont");
+// }
+// menu_btn.addEventListener("click", () => {
+//   sidebar.classList.toggle("active-nav");
+//   container.classList.toggle("active-cont");
+// });
 
-// Theme selection
-var webpage = document.querySelector("body");
-function changeTheme(theme_selector, theme){
-  webpage.className = theme_selector;
-  webpage.style.backgroundImage = "url('img/theme/"+theme+"/main-bg.svg')";
-  sidebar.style.backgroundImage = "url('img/theme/"+theme+"/nb-bg.svg')";;
-} 
+// // Theme selection
+// var webpage = document.querySelector("body");
+// function changeTheme(theme_selector, theme){
+//   webpage.className = theme_selector;
+//   webpage.style.backgroundImage = "url('img/theme/"+theme+"/main-bg.svg')";
+//   sidebar.style.backgroundImage = "url('img/theme/"+theme+"/nb-bg.svg')";;
+// } 
