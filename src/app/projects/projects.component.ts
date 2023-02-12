07a72e0b124ff57105ba37f1f178e9567751b973
ani-poroorkara <anirudh_p@outlook.com>
2023-02-12 14:50:36 -0500

    added final working

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.sass']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+
+  projects : any = [];
+  projects_featured : any = [];
+
+  constructor( private http : HttpClient) { }
+
+  ngOnInit(): void {
+    this.getProjects();
+    this.getFeaturedProjects();
+  }
+  
+  getProjects(){
+    this.http.get('../assets/data/projects/projects.json').subscribe(
+      data => {
+        this.projects = data;
+      }
+    )
+  }
+  getFeaturedProjects(){
+    this.http.get('../assets/data/projects/projects-featured.json').subscribe(
+      data => {
+        this.projects_featured = data;
+      }
+    )
+  }
 
 }
